b75116abe08d23d915fd041596761ecad3ae2d28
ehj <ehj9118@gmail.com>
2025-09-09 09:52:00 +0900

    가위바위보 실습1

---
+++ react/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function GetResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부'}
+
+function handleClick() {
+  console.log('가위 바위 보!');
+}
+
+const me = 'rock'
+const other = 'scissor';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <>
+    <h1>가위바위보</h1>
+    <h2>{GetResult(me, other)}</h2> 
+    <button onClick={handleClick}>가위</button> 
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+   </>
+  </StrictMode>
+)
