604b317ed0a72d377e91b92462e9b09eba6ac943
ehj <ehj9118@gmail.com>
2025-09-15 05:17:13 +0900

    파일 초기화 버튼

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,11 +1,28 @@
+import { useRef } from "react";
+
+
 function FileInput ({name, value, onChange}) {
+  const inputRef = useRef();
 
 const handleChange = (e) => {
     const nextValue = e.target.files[0]
     onChange(name, nextValue);
 };
+
+const handClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = '';
+    onChange(name, null);
+}
+
 return (
-<input name="file" type="file" onChange={handleChange}></input>
+    <div>
+        <input name="file" type="file" onChange={handleChange} ref={inputRef}></input>
+        {value && <button onClick={handClearClick}>X</button>}
+    </div>
+
 );
 }
 
