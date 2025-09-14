6ced91f151f1865f6d6639ccf75546e0c6434511
ehj <ehj9118@gmail.com>
2025-09-15 05:32:32 +0900

    사이드 이펙트 로직 추가

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -19,10 +19,19 @@ const handClearClick = () => {
     onChange(name, null);
 };
 
+//이미지 미리보기 
 useEffect(() => {
     if(!value) return;
     const nextPreview = URL.createObjectURL(value);
     setPreview(nextPreview);
+    
+/* ObjectURL를 해제하는 코드. 사용자가 업로드 변경하거나 취소하는 경우, 
+이전의 사이드 이펙트를 정리함(메모리 정리) */
+    return () => {
+        setPreview();
+        URL.revokeObjectURL(nextPreview)
+    }
+
 }, [value]);
 
 return (
