4d1b8c1da07399764b9e80b4067cd014089fddc2
ehj <ehj9118@gmail.com>
2025-09-15 01:36:17 +0900

    페이지 네이션 오프셋 오류 수정

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
-export async function getFoods() {
-    const response = await fetch('https://learn.codeit.kr/1636/foods')
-    const body = await response.json();
-    return body.foods;
+export async function getFoods({order = 'createdAt', cursor = '', limit=10}) {
+    const query = `?order=${order}&cursor=${cursor}&limit=${limit}`;
+    const response = await fetch(`https://learn.codeit.kr/1636/foods${query}`)
+     const body = await response.json();
+  return {
+    foods: body.foods,
+    paging: body.paging
+  };
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,16 @@ import { getFoods } from '../api';
 import FoodList from './FoodList';
 import { useEffect, useState } from 'react';
 
+const LIMIT =10;
+
 function App() {
   
   const [order, setOrder] = useState('createdAt');
   const [items, setItems] = useState([]);
+  const [cursor, setCursor] = useState('');
+  const [hasNext, setHasNext] = useState(false);
+// 오프셋이 아니라 커서를 사용하고 있어서 오류가 나고 있었음 api,App 파일 로직 맞춰서 수정 
+
   const sortedItems = items.sort((a,b) => b[order] - a[order]);
   
   const handleNewestClink = () => setOrder('createdAt');
@@ -16,22 +22,33 @@ function App() {
     setItems(nextItems);
   }
 
-  const handleLoad = async () => {
-    const foods = await getFoods(); 
-    setItems(foods);
+  const handleLoad = async (options) => {
+    const { foods, paging } = await getFoods(options); 
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems([...items, ...foods]);
+    }
+     setCursor(paging.nextCursor || '');
+    setHasNext(!!paging.nextCursor);
   };
+
+   const handleLoadMore = () => {
+    handleLoad({ order,  cursor, limit:LIMIT });
+  }
 
 /* useEffect 사용 안 하면 무한 호출에 갇히는 수가 있음 
 페이지 처음 렌더링 될때 데이터 불러와서 보여주고 싶으면 useEffect.*/
   useEffect(() => {
-    handleLoad();
-}, []);
+    handleLoad({ order, cursor: '', limit: LIMIT });
+}, [order]); 
 
   return (
     <div> 
       <button onClick={handleNewestClink}>최신순</button>
       <button onClick={handlecalorieClink}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      <button disabled={!hasNext} onClick={handleLoadMore}>더보기</button>
     </div>
   );
 }
