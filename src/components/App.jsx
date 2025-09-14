25e4779870a39eb5cd32df16b84ddad34c2ade2d
ehj <ehj9118@gmail.com>
2025-09-15 02:14:59 +0900

    네트워크 로딩중 로직

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [cursor, setCursor] = useState('');
   const [hasNext, setHasNext] = useState(false);
 // 오프셋이 아니라 커서를 사용하고 있어서 오류가 나고 있었음 api,App 파일 로직 맞춰서 수정 
+  const [ isLoding, setIsLoding ] = useState(false);
 
   const sortedItems = items.sort((a,b) => b[order] - a[order]);
   
@@ -23,11 +24,25 @@ function App() {
   }
 
   const handleLoad = async (options) => {
-    const { foods, paging } = await getFoods(options); 
+    let result;
+    try {
+    setIsLoding(true);
+    result = await getFoods(options);
+    } catch (error) {
+      console.error(error);
+      return;
+    } finally{
+      setIsLoding(false);
+    }
+    
+    const { foods, paging } = result;
+
     if (!options.cursor) {
       setItems(foods);
     } else {
-      setItems([...items, ...foods]);
+      setItems((prevItem) => [...prevItem, ...foods]);
+      /* 삭제 함수 이용 후 불러오기가 작동될 때 삭제되기 이전의 값을 불러오게 됨
+      prev 를 통해 스테이트 값을 내려 받아서 렌더링하도록 함 */
     }
      setCursor(paging.nextCursor || '');
     setHasNext(!!paging.nextCursor);
@@ -41,14 +56,18 @@ function App() {
 페이지 처음 렌더링 될때 데이터 불러와서 보여주고 싶으면 useEffect.*/
   useEffect(() => {
     handleLoad({ order, cursor: '', limit: LIMIT });
-}, [order]); 
+}, [order]); // 정렬기준이 바뀔 때 요청을 새로 보냄.
+//  처음 렌더링 때 요청 두 번 보내는 것은 개발 모드에서 StrictMode 때문에 발생하는 정상 동작. 
 
   return (
     <div> 
       <button onClick={handleNewestClink}>최신순</button>
       <button onClick={handlecalorieClink}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
-      <button disabled={!hasNext} onClick={handleLoadMore}>더보기</button>
+    {hasNext && (
+      <button disabled={isLoding} onClick={handleLoadMore}>더보기</button>
+    )}
+    
     </div>
   );
 }
